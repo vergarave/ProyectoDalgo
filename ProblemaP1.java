15943625560c51c3b4443d86ddb22dac725a2d85
camilacaballer <c.caballero11@uniandes.edu.co>
2025-09-18 15:04:55 -0500

    cambios bobos

--- ProblemaP1.java
+++ ProblemaP1.java
@@ -1,5 +1,8 @@
 
-public class Principal {
+// Daniel Vergara - 202320392
+// Camila Caballero - 202320565
+
+public class ProblemaP1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
