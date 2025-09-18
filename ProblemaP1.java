97820707f5fed7ecb3d7b84c73a49012ca4889b5
camilacaballer <c.caballero11@uniandes.edu.co>
2025-09-18 15:27:45 -0500

    main para entradas y salidas

--- ProblemaP1.java
+++ ProblemaP1.java
@@ -2,8 +2,38 @@
 // Daniel Vergara - 202320392
 // Camila Caballero - 202320565
 
+import java.io.*;
+
 public class ProblemaP1 {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int numCasos = Integer.parseInt(br.readLine());
+
+        ProblemaP1 instance = new ProblemaP1();
+
+        for (int i = 0; i < numCasos; i++) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            int k = Integer.parseInt(tokens[0]);
+            int n = Integer.parseInt(tokens[1]);
+            int[] P = new int[5];
+            for (int j = 0; j < 5; j++) {
+                P[j] = Integer.parseInt(tokens[2 + j]);
+            }
+
+            int resp = instance.resolverCaso(k, n, P);
+
+            System.out.println(resp);
+        }
     }
+
+    public int resolverCaso(int k, int n, int[] P) {
+        // TODO: implementar la DP para maximizar creatividad
+        return 0; //placeholder
+    }
+
+
 }
